33aaef67c66b24cbe3c163c888036b397992fdf8
かいかいのまくだなるず <k-aoyama@mac-kaikai.local>
2021-01-24 14:48:28 +0900

    ransack実装完了

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.create(title: "タイトルA", favorite_count: 10)
+Post.create(title: "タイトルB", favorite_count: 20)
+Post.create(title: "タイトルC", favorite_count: 30)
+Post.create(title: "タイトルD", favorite_count: 40)
+Post.create(title: "タイトルE", favorite_count: 50)
